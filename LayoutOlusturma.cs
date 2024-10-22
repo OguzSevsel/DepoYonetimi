988daac72c602b026c1b3cd9f6c16a6ed659438a
OguzSevsel <oguzsevsel@gmail.com>
2024-10-22 23:17:52 +0300

    asd

--- LayoutOlusturma.cs
+++ LayoutOlusturma.cs
@@ -43,6 +43,8 @@ namespace Balya_Yerleştirme
         public float UnchangedDepoAlaniBoyu { get; set; }
         public float UnchangedConveyorEni { get; set; }
         public float UnchangedConveyorBoyu { get; set; }
+        public float UnchangedConveyorBoyu1 { get; set; }
+
 
         List<RectangleF> rectangles = new List<RectangleF>();
 
